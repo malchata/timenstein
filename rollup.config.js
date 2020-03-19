658d3051475a3c95947e93ce89f6871234762d45
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2020-03-19 13:40:25 -0500

    Kind of works. Refactored to a different object pattern. Needs more work, though.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,86 @@
 /* eslint-env node */
+import babel from "rollup-plugin-babel";
+import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
-export default {
-  input: pkg.module,
-  output: {
-    file: pkg.main,
-    format: "cjs"
+const commonTerserOptions = {
+  timings: true,
+  compress: {
+    sequences: true,
+    conditionals: true,
+    evaluate: true,
+    unsafe_arrows: true,
+    warnings: true
   }
 };
+
+export default [
+  {
+    input: pkg.module,
+    output: {
+      file: "dist/timenstein.min.mjs",
+      format: "esm"
+    },
+    plugins: [
+      babel({
+        presets: [
+          [
+            "@babel/preset-env", {
+              targets: {
+                esmodules: true
+              },
+              loose: true
+            }
+          ]
+        ]
+      }),
+      terser({
+        ecma: 8,
+        mangle: {
+          keep_fnames: true,
+          toplevel: true,
+          reserved: ["timenstein"],
+          module: true
+        },
+        ...commonTerserOptions
+      })
+    ]
+  },
+  {
+    input: pkg.module,
+    output: {
+      name: "timenstein",
+      file: "dist/timenstein.min.js",
+      format: "iife"
+    },
+    plugins: [
+      babel({
+        presets: [
+          [
+            "@babel/preset-env", {
+              targets: ">0.5%, last 5 versions, ie > 10, not dead",
+              loose: true
+            }
+          ]
+        ]
+      }),
+      terser({
+        ecma: 5,
+        mangle: {
+          keep_fnames: true,
+          toplevel: true,
+          reserved: ["Timenstein"],
+          module: false
+        },
+        ...commonTerserOptions
+      })
+    ]
+  },
+  {
+    input: pkg.module,
+    output: {
+      file: pkg.main,
+      format: "cjs"
+    }
+  }
+];
--- src/timenstein.mjs
+++ src/timenstein.mjs
@@ -1,117 +1,108 @@
-function Timenstein (options) {
-  options = options || {};
+const Timenstein = (function () {
+  // Private variables
+  const compatible = "performance" in window && "mark" in window.performance && "measure" in window.performance && "getEntriesByName" in window.performance && "getEntriesByType" in window.performance && "clearMarks" in window.performance;
+  let marks = {}, measures = {};
+  let namespace;
 
-  this.namespace = "namespace" in options ? options.namespace : "timenstein";
-  this.log = "logging" in options ? options.log : false;
-  this.logLevel = "logLevel" in options ? options.logLevel : "warn";
+  // Private methods
+  const clone = obj => JSON.parse(JSON.stringify(obj));
 
-  this.compatible = "performance" in window && "mark" in window.performance && "measure" in window.performance && "getEntriesByName" in window.performance && "getEntriesByType" in window.performance && "clearMarks" in window.performance;
-  this.marks = {};
-  this.measures = {};
-}
+  // Constructor
+  function Timenstein (options) {
+    options = options || {};
 
-Timenstein.prototype.mark = function (measureName) {
-  // Nothing will happen if the APIs we need aren't available. Also, if
-  // a name for the measure we want to make isn't specified, nothing happens.
-  if (measureName && this.compatible) {
-    let markNumber, markName;
-
-    // Check to see if this measure has been started
-    if (measureName in this.marks) {
-      markNumber = this.marks[measureName].length + 1;
-    } else {
-      this.marks[measureName] = [];
-      markNumber = 1;
-    }
-
-    // Construct the mark name and create a performance mark
-    markName = `${this.namespace}:${measureName}-${markNumber}`;
-    performance.mark(markName);
-
-    // Create a custom object from the mark we just, er, marked.
-    const markEntry = this.clone(performance.getEntriesByName(markName)[0]);
-
-    // Record the mark in our marks array.
-    this.marks[measureName].push(markEntry);
-
-    return markEntry;
+    namespace = "namespace" in options ? options.namespace : "timenstein";
   }
 
-  return false;
-};
+  // Public methods
+  Timenstein.prototype.mark = function (measureName) {
+    if (measureName && compatible) {
+      let markNumber, markName;
 
-Timenstein.prototype.measure = function (measureName, from, to) {
-  // Once again, we aren't doing anything unless the APIs we need are available.
-  // We also need necessary parameters in order to calculate the measure, and
-  // ensure that the given measureName has at least two recorded marks.
-  if (measureName && this.compatible && measureName in this.marks && this.marks[measureName].length >= 2) {
-    const markCount = this.marks[measureName].length;
-
-    // If the `from` and `to` arguments aren't provided, just measure from end
-    // to end in the measure's array.
-    from = from || 1;
-    to = to || this.marks[measureName].length;
-
-    // Make sure the `from` value is in bounds
-    if (from < 1 || from > to || from > markCount) {
-      return false;
-    }
-
-    // Make sure the `to` value is in bounds
-    if (to < 1 || to < from || to > markCount) {
-      return false;
-    }
-
-    const namespacedMeasureName = `${this.namespace}:${measureName}`;
-    const startMark = performance.getEntriesByName(`${measureName}-${from}`)[0];
-    const endMark = performance.getEntriesByName(`${measureName}-${to}`)[0];
-
-    if (namespacedMeasureName in this.measures === false) {
-      this.measures[namespacedMeasureName] = [];
-    }
-
-    performance.measure(namespacedMeasureName, startMark, endMark);
-    const measureEntry = this.clone(performance.getEntriesByName(namespacedMeasureName)[0]);
-    this.measures[namespacedMeasureName].push(measureEntry);
-
-    return measureEntry;
-  }
-
-  return false;
-};
-
-Timenstein.prototype.clear = function (what) {
-  if (what === "marks" || what === "measures") {
-    const method = `clear${what[0].toUpperCase() + what.slice(1)}`;
-
-    performance.getEntries().forEach(entry => {
-      if (entry.entryType === what.substr(0, what.length - 1) && entry.name.indexOf(this.namespace) > -1) {
-        performance[method](entry.name);
+      // Check to see if this measure has been started
+      if (measureName in marks) {
+        markNumber = marks[measureName].length + 1;
+      } else {
+        marks[measureName] = [];
+        markNumber = 1;
       }
-    });
 
-    return true;
-  }
+      // Construct the mark name and create a performance mark
+      markName = `${namespace}:${measureName}-${markNumber}`;
+      performance.mark(markName);
 
-  return false;
-};
+      // Create a custom object from the mark we just, er, marked.
+      const markEntry = clone(performance.getEntriesByName(markName)[0]);
 
-Timenstein.prototype.clone = function (obj) {
-  if (typeof obj === "object") {
-    return JSON.parse(JSON.stringify(obj));
-  }
+      // Record the mark in our marks array.
+      marks[measureName].push(markEntry);
 
-  return false;
-};
+      return markEntry;
+    }
 
-Timenstein.prototype.get = function (what) {
-  if (what === "marks") {
-    return this.marks;
-  } else if (what === "measures") {
-    return this.measures;
-  }
+    return false;
+  };
 
-  return false;
-};
+  Timenstein.prototype.measure = function (measureName, from, to) {
+    if (measureName && compatible && measureName in marks && marks[measureName].length >= 2) {
+      // Set up param defaults
+      from = from || 1;
+      to = to || marks[measureName].length;
 
-//export default Timenstein;
+      const markCount = marks[measureName].length;
+
+      // Make sure the `from` and `to` values are in bounds
+      if (from < 1 || from > to || from > markCount) {
+        return false;
+      }
+
+      if (to < 1 || to < from || to > markCount) {
+        return false;
+      }
+
+      const namespacedMeasureName = `${namespace}:${measureName}`;
+      const startMark = performance.getEntriesByName(`${measureName}-${from}`)[0];
+      const endMark = performance.getEntriesByName(`${measureName}-${to}`)[0];
+
+      if (namespacedMeasureName in measures === false) {
+        measures[namespacedMeasureName] = [];
+      }
+
+      performance.measure(namespacedMeasureName, startMark, endMark);
+      const measureEntry = clone(performance.getEntriesByName(namespacedMeasureName)[0]);
+      measures[namespacedMeasureName].push(measureEntry);
+
+      return measureEntry;
+    }
+
+    return false;
+  };
+
+  Timenstein.prototype.clear = function (what) {
+    if (what === "marks" || what === "measures") {
+      const method = `clear${what[0].toUpperCase() + what.slice(1)}`;
+
+      performance.getEntries().forEach(entry => {
+        if (entry.entryType === what.substr(0, what.length - 1) && entry.name.indexOf(namespace) > -1) {
+          performance[method](entry.name);
+        }
+      });
+
+      return true;
+    }
+
+    return false;
+  };
+
+  Timenstein.prototype.getMarks = function () {
+    return marks;
+  };
+
+  Timenstein.prototype.getMeasures = function () {
+    return measures;
+  };
+
+  return Timenstein;
+}());
+
+export default Timenstein;
